8825749efeb239574b7b9457ae724ff43fdbed4f
tjsgnrla97 <tjsgnrla97@gmail.com>
2022-02-14 18:04:36 +0900

    Create Study06.java

---
+++ GameTest/src/slime/study/Study06.java
@@ -0,0 +1,10 @@
+package slime.study;
+
+public class Study06 {
+
+	public static void main(String[] args) {
+		System.out.println(1 < 3);
+		System.out.println(3 < 3);
+		System.out.println(3 <= 3);
+	}
+}
